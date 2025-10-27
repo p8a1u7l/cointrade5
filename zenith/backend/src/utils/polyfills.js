fb2d48cab0c7e268afe516b67ed24a3b04133d73
p8a1u7l <rtpk6818@gmail.com>
2025-10-27 23:52:56 +0900

    Fix undici polyfill imports for CommonJS

--- zenith/backend/src/utils/polyfills.js
+++ zenith/backend/src/utils/polyfills.js
@@ -1,29 +1,39 @@
-import { fetch as undiciFetch, Headers, Request, Response, FormData, File, Blob } from 'undici';
+import undici from 'undici';
 
-if (typeof globalThis.fetch !== 'function') {
+const {
+  fetch: undiciFetch,
+  Headers: UndiciHeaders,
+  Request: UndiciRequest,
+  Response: UndiciResponse,
+  FormData: UndiciFormData,
+  File: UndiciFile,
+  Blob: UndiciBlob,
+} = undici;
+
+if (typeof globalThis.fetch !== 'function' && typeof undiciFetch === 'function') {
   globalThis.fetch = undiciFetch;
 }
 
-if (typeof globalThis.Headers !== 'function') {
-  globalThis.Headers = Headers;
+if (typeof globalThis.Headers !== 'function' && typeof UndiciHeaders === 'function') {
+  globalThis.Headers = UndiciHeaders;
 }
 
-if (typeof globalThis.Request !== 'function') {
-  globalThis.Request = Request;
+if (typeof globalThis.Request !== 'function' && typeof UndiciRequest === 'function') {
+  globalThis.Request = UndiciRequest;
 }
 
-if (typeof globalThis.Response !== 'function') {
-  globalThis.Response = Response;
+if (typeof globalThis.Response !== 'function' && typeof UndiciResponse === 'function') {
+  globalThis.Response = UndiciResponse;
 }
 
-if (typeof globalThis.FormData !== 'function') {
-  globalThis.FormData = FormData;
+if (typeof globalThis.FormData !== 'function' && typeof UndiciFormData === 'function') {
+  globalThis.FormData = UndiciFormData;
 }
 
-if (typeof globalThis.File !== 'function') {
-  globalThis.File = File;
+if (typeof globalThis.File !== 'function' && typeof UndiciFile === 'function') {
+  globalThis.File = UndiciFile;
 }
 
-if (typeof globalThis.Blob !== 'function') {
-  globalThis.Blob = Blob;
+if (typeof globalThis.Blob !== 'function' && typeof UndiciBlob === 'function') {
+  globalThis.Blob = UndiciBlob;
 }
